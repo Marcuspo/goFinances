816b8ff2758e52a60e22440237a1418e4d6d0562
Marcus Pontes <marcusporama@gmail.com>
2022-01-04 16:16:15 -0300

    update

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Container, Title,
+	Amount,
+	Footer,
+	Category,
+	Icon,
+	CategoryName,
+	Date } from './styles'
+
+
+
+export function TransactionCard(){
+	return(
+		<Container>
+			<Title>Desenvolvimento de site</Title>
+			<Amount>R$12.000</Amount>
+
+			<Footer>
+				<Category>
+					<Icon name="dollar-sign"/>
+					<CategoryName>Vendas</CategoryName>
+				</Category>
+				<Date>13/12/2021</Date>
+			</Footer>
+		</Container>
+	)
+}
